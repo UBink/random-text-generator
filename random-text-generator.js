b0e9ed8e2d5668e58991d4f55375bc62850d3131
UBink <140355454+UBink@users.noreply.github.com>
2023-09-14 14:27:13 -0700

    Add files via upload

--- random-text-generator.js
+++ random-text-generator.js
@@ -1,38 +1,43 @@
-//going for a star reading theme. creating arrays that have the words that will be randomized
-const stars = ['Polaris', 'Sirius', 'Alpha Centauri System', "Betelgeuse", 'Rigel', 'Vega', 'Pleiades', 'Antares', 'Canopus'];
-const reading = ["Good things are coming", "Be careful", "Oh! so bad I can't even start", 'You achieve great things in the future']
-const food = ['sushi', 'burger', 'salad', 'desert']
+// Add an event listener to the button
+document.getElementById('generateButton').addEventListener('click', () => {
+    const stars = ['Polaris', 'Sirius', 'Alpha Centauri System', "Betelgeuse", 'Rigel', 'Vega', 'Pleiades', 'Antares', 'Canopus'];
+    const reading = ["Good things are coming", "Be careful", "Oh! so bad I can't even start", 'You achieve great things in the future'];
+    const food = ['sushi', 'burger', 'salad', 'dessert'];
 
-//creating the randomizer function
+    
+    const output = document.getElementById('output');
+    output.innerHTML = randomizer(stars, reading, food) + ;
+});
+// Your randomizer function with corrected loop condition
 const randomizer = (arr1, arr2, arr3) => {
-    //creating empty array that will contain the randomized message
-    let container =[];
-    //using loops to have a possibilty of getting multiple stars
-     for (let i = Math.floor(Math.random() * arr1.length); i <= arr1.length; i++ ) {
-        container.push( arr1[i]);
-    }
-    //creating conditionals to correct grammar when we get multiple stars
-    if (container.length >= 3) {
-        console.log('Your lucky stars are: ')
-        
-     } else if(container.length <= 2) {
-        console.log('Your lucky star is: ')
-     }
-    //randomizing the second array
+    // Creating an empty array that will contain the randomized message
+    var container = [];
+    // Using loops to have a possibility of getting multiple stars
+    let starContainer = []
+        for (let i = Math.floor(Math.random() * arr1.length); i < arr1.length; i++) {
+           starContainer.push(arr1[i]);
+        }
+        if (starContainer.length >= 3) {
+            container.push('Your lucky stars are:');
+        } else if (starContainer.length <= 2) {
+            container.push('Your lucky star is:');
+        }
+    
+    container.push(starContainer)
+    // Creating conditionals to correct grammar when we get multiple stars
+    
+    // Randomizing the second array
     let j = Math.floor(Math.random() * arr2.length);
-     //creating conditionals to correct grammar when we get multiple stars
+    // Creating conditionals to correct grammar when we get multiple stars
     if (container.length >= 3) {
         container.push('Your stars say: ' + arr2[j]);
-    } else if( container.length <= 2){
+    } else if (container.length <= 2) {
         container.push('Your star says: ' + arr2[j]);
     }
-    //randomizing the third array
-    let l = Math.floor(Math.random() * food.length);
-    container.push('Your lucky food is: ' + food[l]);
-    
-     //returning the randomized array
-     return container.join('\n');
-}
+    // Randomizing the third array
+    let l = Math.floor(Math.random() * arr3.length);
+    container.push('Your lucky food is: ' + arr3[l]);
 
-
-console.log(randomizer(stars, reading, food));
+    // Returning the randomized array
+    return container.join('\n');
+};
